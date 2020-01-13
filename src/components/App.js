93fdae9fd758d8472b032e541c2375f78671554f
anapayanruiz <anapayan22@gmail.com>
2020-01-13 10:04:45 +0100

    Add Router and Switch to App

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import Header from './Header';
 import Filters from './Filters';
 import CharacterList from './CharacterList';
 import fetchCharacters from '../services/fetchCharacters';
+import { Route, Switch } from 'react-router-dom';
+import CharacterDetail from './CharacterDetail';
+
 
 
 class App extends React.Component {
@@ -30,7 +33,14 @@ class App extends React.Component {
       <div className="page">
         <Header />
         <Filters />
-        <CharacterList characters={this.state.characters} />
+        <Switch>
+          <Route exact path='/'>
+            <CharacterList characters={this.state.characters} />
+          </Route>
+          <Route>
+            <CharacterDetail path='/characters/:id' />
+          </Route>
+        </Switch>
       </div>
     );
   }
