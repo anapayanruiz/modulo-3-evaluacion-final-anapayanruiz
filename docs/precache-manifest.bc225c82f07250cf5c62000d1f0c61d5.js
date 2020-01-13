dc68d4e0905c331250d636ac4b74f982a0220957
anapayanruiz <anapayan22@gmail.com>
2020-01-13 08:39:29 +0100

    Update docs

---
+++ docs/precache-manifest.bc225c82f07250cf5c62000d1f0c61d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eded7a0e702344564f335dfc28c89b24",
+    "url": "./index.html"
+  },
+  {
+    "revision": "893516060b458c70ba55",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1786f55f1c18b6d0b455",
+    "url": "./static/js/2.b7c16523.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.b7c16523.chunk.js.LICENSE"
+  },
+  {
+    "revision": "893516060b458c70ba55",
+    "url": "./static/js/main.feb9ba1f.chunk.js"
+  },
+  {
+    "revision": "33cbce518cfce9d077f9",
+    "url": "./static/js/runtime-main.968c2cc7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
